eca10b3b14e650ff0bc8bca55db9449208d09c64
Giada <tonni.giada.210802@gmail.com>
2025-12-03 15:18:51 +0100

    LAVORO CONCLUSO, C'E' UN PICCOLO BUG CON LE ICONE DEI FAV

---
+++ d9-redux-main/src/components/FavList.jsx
@@ -0,0 +1,23 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import SingleFav from './SingleFav'
+
+const FavList = function () {
+  const favList = useSelector((currentState) => {
+    return currentState.favouritesList.content
+  })
+
+  return (
+    <Container className="my-5">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          {favList.map((element, i) => {
+            return <SingleFav key={i} fav={element} />
+          })}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default FavList
--- d9-redux-main/src/components/Job.jsx
+++ d9-redux-main/src/components/Job.jsx
@@ -1,20 +1,59 @@
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { FaHeart, FaRegHeart } from 'react-icons/fa'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-)
+const Job = ({ data }) => {
+  const favList = useSelector((currentState) => {
+    return currentState.favouritesList.content
+  })
+
+  const getName = function (name) {
+    if (favList.includes(name)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const [icona, setIcona] = useState(getName(data.company_name))
+  const dispatch = useDispatch()
+
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: '1px solid #00000033', borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+        <button
+          className="bg-transparent border-0 fs-4"
+          onClick={() => {
+            setIcona(!icona)
+            if (icona) {
+              dispatch({
+                type: 'REMOVE_FROM_FAVLIST',
+                payload: data.company_name,
+              })
+            } else {
+              dispatch({
+                type: 'ADD_TO_FAVLIST',
+                payload: data.company_name,
+              })
+            }
+          }}
+        >
+          {icona ? <FaHeart /> : <FaRegHeart />}
+        </button>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  )
+}
 
 export default Job
